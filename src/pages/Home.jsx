c5b4310b1c54074a057678720a6539535b1df794
AdrianoR85 <adrianorosa085@gmail.com>
2024-03-28 15:16:35 -0300

    Home page update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
 /* eslint-disable react/no-unescaped-entities */
+import { FaGithub, FaLinkedin} from "react-icons/fa6";
 import '../styles/home.css'
 export default function Home() {
   return (
     <section id="home" className='home-wrapper'>
       <div className="home__description">
-        <span className='home__span'>Hello, I'm</span>
         <h1>Adriano Rosa </h1>
-        <p>I´m a<span> Systems Analysis and Development </span>student</p>
+        <p>Estudante de Análise e Desenvolvimento de Sistemas </p>
+        <div className="home__description--links">
+          <a href="https://github.com/AdrianoR85" target="_blank"><FaGithub /></a>
+          <a href="mhttps://www.linkedin.com/in/adriano-rosa-741979182/" target="_blank"><FaLinkedin /></a>
+          <a href="./Adrino_Rosa_CV.pdf" download className="home__description--cv">Download_CV</a>
+        </div>
+        
       </div>
       <figure className="home__picture">
         <img src="http://github.com/AdrianoR85.png" alt="my photo" />
